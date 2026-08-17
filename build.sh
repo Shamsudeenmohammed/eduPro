2baf41b525c73bf7e67bc78cfdb561af30c69218
Shamsudeenmohammed <mshamsudeen551@gmail.com>
2026-08-17 07:06:34 +0000

    updated build command

--- build.sh
+++ build.sh
@@ -1,6 +1,3 @@
 #!/usr/bin/env bash
 set -o errexit
-
-pip install -r requirements.txt
-python manage.py collectstatic --no-input
-python manage.py migrate
+pip install -r requirements.txt && python manage.py migrate && python manage.py collectstatic --noinput && python manage.py createsruperuser --username $ADMIN_USERNAME --email $ADMIN_EMAIL --password $ADMIN_PASSWORD --noinput
