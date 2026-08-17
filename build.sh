0017de105f209ecd1836adac2eb490a9617b00b9
Shamsudeenmohammed <mshamsudeen551@gmail.com>
2026-08-17 07:30:28 +0000

    Remove some build command

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 set -o errexit
-pip install -r requirements.txt && python manage.py migrate && python manage.py collectstatic --noinput && python manage.py createsuperuser --username $ADMIN_USERNAME --email $ADMIN_EMAIL --password $ADMIN_PASSWORD --noinput
+pip install -r requirements.txt && python manage.py migrate && python manage.py collectstatic --noinput && python manage.py createsuperuser
